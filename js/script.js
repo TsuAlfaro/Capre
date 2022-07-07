6ac51f09dd7aea8f35dbd49dfc6b133d94761f0d
TsuAlfaro <104867056+TsuAlfaro@users.noreply.github.com>
2022-07-07 13:40:11 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const cCaptions = document.querySelector('.carousel-caption');
 
 const tl = new TimelineMax();
 
-tl.from(brand, 1, {opacity:0, x:-100}, {opacity:1, x:0}, "-=0.5" )
+tl.from(brand, 1, {opacity:0, y:-100}, {opacity:1, y:0}, "-=0.5" )
 .from(menu, 1, {opacity:0}, {opacity:1}, "-=0.5")
 .from(slide, 1, {width:0}, {width:100},"-=0.5" )
 .from(cCaptions, 1.5, {opacity:0}, {opacity:1}, "-=2")
