4abd2deb67c66a8294e06fe40ece3244182cb76b
TsuAlfaro <104867056+TsuAlfaro@users.noreply.github.com>
2022-07-07 14:25:51 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ const hamburger = document.querySelector('.navbar-toggler');
 
 const tl = new TimelineMax();
 
-tl.from(brand, 1, {opacity:0, y:-100}, {opacity:1, y:0}, "-=1.5" )
-.from(menu, 1, {opacity:0}, {opacity:1}, "-=1.5")
-.from(hamburger, 1, {opacity:0, y:-100}, {opacity:1, y:0}, "-=1.5" )
-.from(slide, 1, {width:0}, {width:100},"-=0.5" )
-.from(cCaptions, 1, {opacity:0}, {opacity:1}, "-=2")
+tl.from(slide, 1, {width:0}, {width:100},"+=0.5")
+.from(brand, 1, {opacity:0, y:-100}, {opacity:1, y:0},"+=1")
+.from(menu, 1, {opacity:0}, {opacity:1},"+=0.5")
+.from(hamburger, 1, {opacity:0, y:-100}, {opacity:1, y:0}, "+=0.5" )
+.from(cCaptions, 1, {opacity:0}, {opacity:1}, "+=1")
 
 ;
