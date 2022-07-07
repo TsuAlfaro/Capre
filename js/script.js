f8ed31335bcf11c413949ea20e6d2eac6313ffe5
TsuAlfaro <104867056+TsuAlfaro@users.noreply.github.com>
2022-07-07 13:36:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const brand = document.querySelector('.navbar-brand');
 const menu = document.querySelector('.navbar-nav');
 const slide = document.querySelector('.slider');
+const cCaptions = document.querySelector('.carousel-caption');
+
 
 const tl = new TimelineMax();
 
 tl.from(brand, 1, {opacity:0}, {opacity:1}, "-=0.5" )
 .from(menu, 1, {opacity:0}, {opacity:1}, "-=0.5")
-.from(slide,0.5, {width:0}, {width:100},"-=0.5" )
+.from(slide, 1, {width:0}, {width:100},"-=0.5" )
+.from(cCaptions, 1.5, {opacity:0}, {opacity:1}, "-=2")
 ;
