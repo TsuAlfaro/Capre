e7ec857b26014178bc2b9a69117df15672a5dc0b
TsuAlfaro <104867056+TsuAlfaro@users.noreply.github.com>
2022-07-07 13:33:16 -0500

    Update scrollTrigger.js

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -3,7 +3,10 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.from(".introF1", {
   scrollTrigger:{ 
     trigger: ".introF1",
-    toggleractions: "restart none none none"
+    start: "top center",
+    end: "top 50px",
+    scrub: 1,
+    markers:false
   },
   x: -600,
   opacity: 0,
@@ -11,9 +14,13 @@ gsap.from(".introF1", {
 });
 
 gsap.from(".introF2", {
-  scrollTrigger: ".introF2",
+  scrollTrigger:{ 
+    trigger: ".introF2",
+    start: "top center",
+    end: "top 50px",
+    scrub: 1
+  },
   x: 1000,
   opacity: 0,
   duration: 1
 });
-
