2524d356852690d0b7b1f10fd480704cbb946b3e
TsuAlfaro <104867056+TsuAlfaro@users.noreply.github.com>
2022-07-07 13:15:31 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const slide = document.querySelector('.slider');
 
 const tl = new TimelineMax();
 
-tl.from(brand, 2, {opacity:0, y:-50}, {opacity:1, y: 0}, "-=0.5" )
-.from(menu, 2, {opacity:0, y:-50}, {opacity:1, y:0}, "-=0.5")
-.from(slide,1, {width:0}, {width:100},"-=0.5" )
+tl.from(brand, 1, {opacity:0, y:-50}, {opacity:1, y: 0}, "-=0.5" )
+.from(menu, 1, {opacity:0, y:-50}, {opacity:1, y:0}, "-=0.5")
+.from(slide,0.5, {width:0}, {width:100},"-=0.5" )
 ;
