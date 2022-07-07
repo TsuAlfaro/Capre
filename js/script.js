ecd3042439e88f6fb47f160b207001c106722b42
TsuAlfaro <104867056+TsuAlfaro@users.noreply.github.com>
2022-07-07 13:46:46 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@ const brand = document.querySelector('.navbar-brand');
 const menu = document.querySelector('.navbar-nav');
 const slide = document.querySelector('.slider');
 const cCaptions = document.querySelector('.carousel-caption');
-
+const hamburger = document.querySelector('.navbar-toggler');
 
 const tl = new TimelineMax();
 
-tl.from(brand, 1, {opacity:0, y:-100}, {opacity:1, y:0}, "-=0.5" )
-.from(menu, 1, {opacity:0}, {opacity:1}, "-=0.5")
+tl.from(brand, 1, {opacity:0, y:-100}, {opacity:1, y:0}, "-=1.5" )
+.from(menu, 1, {opacity:0}, {opacity:1}, "-=1.5")
+.from(hamburger, 1, {opacity:0, y:-100}, {opacity:1, y:0}, "-=1.5" )
 .from(slide, 1, {width:0}, {width:100},"-=0.5" )
-.from(cCaptions, 1.5, {opacity:0}, {opacity:1}, "-=2")
+.from(cCaptions, 1, {opacity:0}, {opacity:1}, "-=2")
+
 ;
