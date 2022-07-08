b89ade28631550bfd5776cf13cbfddc96d74ce2c
TsuAlfaro <104867056+TsuAlfaro@users.noreply.github.com>
2022-07-08 14:15:20 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@ const menu = document.querySelector('.navbar-nav');
 const slide = document.querySelector('.slider');
 const cCaptions = document.querySelector('.carousel-caption');
 const hamburger = document.querySelector('.navbar-toggler');
+const nosImg = document.querySelector('.img-nos');
 
 const tl = new TimelineMax();
 
-tl.from(slide, 1, {width:0}, {width:100},"+=0.5")
+tl.from(slide, 1, {height:0}, {height:100},"+=0.5")
 .from(brand, 1, {opacity:0, y:-100}, {opacity:1, y:0},"+=1")
-.from(menu, 1, {opacity:0}, {opacity:1},"+=0.5")
+.from(menu, 1, {opacity:0, y:-100}, {opacity:1, y:0},"+=0.5")
 .from(hamburger, 1, {opacity:0, y:-100}, {opacity:1, y:0}, "+=0.5" )
 .from(cCaptions, 1, {opacity:0}, {opacity:1}, "+=1")
-
+.from(nosImg, 1, {opacity:0, y:100}, {opacity:1, y:0},"+=0.5")
 ;
--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -1,4 +1,33 @@
+const animate = gsap.utils.toArray('.AnimText');
+const mov1 = gsap.utils.toArray('.movder');
+
 gsap.registerPlugin(ScrollTrigger);
+
+animate.forEach(AnimText => {
+  gsap.from(AnimText, { 
+    opacity: 0,
+    duration: 0.5,
+    scrollTrigger: {
+      trigger: AnimText,
+      start: "top center",
+        end: "top 50px",
+      scrub: true,
+      markers:true
+    }
+  })
+});
+
+mov1.forEach(movder => {
+  gsap.from(movder, { 
+   x: -100,
+    duration: 1,
+    scrollTrigger: {
+      trigger: movder,
+      start: "top bottom=0px",
+      scrub: true
+    }
+  })
+});
 
 gsap.from(".introF1", {
   scrollTrigger:{ 
@@ -8,7 +37,7 @@ gsap.from(".introF1", {
     scrub: 1,
     markers:false
   },
-  x: -600,
+  x: -100,
   opacity: 0,
   duration: 1
 });
@@ -20,7 +49,7 @@ gsap.from(".introF2", {
     end: "top 50px",
     scrub: 1
   },
-  x: 1000,
+  x: 50,
   opacity: 0,
   duration: 1
 });
